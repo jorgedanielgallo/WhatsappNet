6966f1a4c471634223d1f302b454f598b53fbe93
Jorge Gallo <gallojorge@outlook.com>
2024-03-15 12:58:11 -0500

    Add project files.

---
+++ WhatsappNet.API/Controllers/WhatsAppController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WhatsappNet.API.Controllers
+{
+    [ApiController]
+    [Route("api/whatsapp")]
+    public class WhatsAppController : Controller
+    {
+        [HttpGet("test")]
+        public IActionResult Sample()
+        {
+            return Ok("ok sample");
+        }
+
+        [HttpGet]
+        public IActionResult VerifyToken()
+        {
+            string AccessToken = "ASDASDASDAS9324JASFDASD";
+
+            var token = Request.Query["hub.verify_token"].ToString();
+            var challenge = Request.Query["hub.challenge"].ToString();
+
+            if(challenge != null && token != null && token == AccessToken)
+            {
+                return Ok(challenge);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ReceivedMessage([FromBody])
+    }
+}
